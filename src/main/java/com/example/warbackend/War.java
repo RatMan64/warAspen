a624244dc9335471de8b2b269b9eeadff76f0ff1
RileyRat5x <35906066+RileyRat5x@users.noreply.github.com>
2022-05-10 10:36:27 -0700

    added shuffle_deck_Start and it sets up a new game of war

--- src/main/java/com/example/warbackend/War.java
+++ src/main/java/com/example/warbackend/War.java
@@ -1,15 +1,56 @@
 package com.example.warbackend;
 
 import java.security.PublicKey;
+import java.util.Arrays;
+import java.util.LinkedList;
 import java.util.Queue;
+import java.util.Random;
 
 public class War {
-  private Queue humanplayer;
-  private  Queue npc1;
-  private Queue npc2;
-
-
+  private LinkedList<Integer> humanplayer;
+  private  LinkedList<Integer> npc1;
+  private LinkedList<Integer> npc2;
+  LinkedList<Integer> deck;
   public War(){
+
+
+    deck = new LinkedList<Integer>(Arrays.asList(1,1,1,1,2,2,2,2,3,3,3,3,4,4,4,4,5,5,5,5,6,6,6,6,7,7,7,7,8,8,8,8,9,9,9,9,10,10,10,10,11,11,11,11,12,12,12,12,13,13,13,13));
+    npc1 = new LinkedList<Integer>();
+    npc2 = new LinkedList<Integer>();
+    humanplayer = new LinkedList<Integer>();
+    shuffle_deck_Start();
+    play_round();
+  }
+  public War(int[] human, int[] p1,int[] p2){ // if a game is already started
+    humanplayer = new LinkedList<Integer>();
+
+  }
+  public void play_round(){
+    System.out.println( "firstplay");
+  }
+
+  public void shuffle_deck_Start(){
+
+    Random rand = new Random();
+    while(deck.size() > 0){
+
+      if(deck.size()<=0)break;
+      int nextcard = rand.nextInt(deck.size());
+      humanplayer.add(deck.remove(nextcard));
+
+      if(deck.size()<=0)break;
+      nextcard = rand.nextInt(deck.size());
+      npc1.add(deck.remove(nextcard));
+
+      if(deck.size()<=0)break;
+      nextcard = rand.nextInt(deck.size());
+      npc2.add(deck.remove(nextcard));
+    }
+    // at this point the deck should be shuffled if this is a new game
+
+  }
+
+  public void getQueue(){
 
   }
 
--- src/main/java/com/example/warbackend/WarController.java
+++ src/main/java/com/example/warbackend/WarController.java
@@ -13,12 +13,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class WarController {
 
-  War game = new War();
+
 
 
   @GetMapping("/war")
   public War wargame(@RequestParam(value = "name", defaultValue = "null") String player,@RequestParam(value = "p1", defaultValue = "null")String p1,@RequestParam(value = "p2", defaultValue = "null")String p2,@RequestParam(value = "p3", defaultValue = "null")String p3){
-     //
+     System.out.println("this is what was entered: "+ "player: "+ player+ " p1: "+ p1+" p2: "+ p2+" p3: "+ p3);
+     War wp = new War();
 
     return null;
   }
@@ -26,7 +27,7 @@ public class WarController {
 
   @GetMapping("/getwinsfor")
   public String getWins(@RequestParam(value = "id")String query){
-    System.out.println(query);
+
     Wins wp = new Wins(query);
 
     return "{ id: "+query+", wins: "+wp.getWins()+"}";
